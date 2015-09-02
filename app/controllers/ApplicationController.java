dd235dc185e1fe73c6341bba4670ab43916a46a0
Peter Gerhard <peter.gerhard@commercetools.de>
2015-09-02 17:06:02 +0200

    Setup Home Page

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import common.cms.CmsPage;
 import common.controllers.ControllerDependency;
 import common.controllers.SunriseController;
+import common.pages.SunrisePageData;
 import play.libs.F;
 import play.mvc.Result;
+import productcatalog.pages.HomePageContent;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -20,6 +23,12 @@ public final class ApplicationController extends SunriseController {
     }
 
     public F.Promise<Result> index() {
-        return F.Promise.pure(ok("Sunrise Home"));
+        return getCmsPage("home").map(this::getResult);
+    }
+
+    private Result getResult(final CmsPage cms) {
+        final HomePageContent content = new HomePageContent();
+        final SunrisePageData pageData = SunrisePageData.of(cms, context(), content);
+        return ok(templateService().renderToHtml("home", pageData));
     }
 }
