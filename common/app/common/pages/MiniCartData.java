2ff06f53279b0ee41b7cc9fc6ceb0f7dc3e53364
Peter Gerhard <peter.gerhard@commercetools.de>
2015-09-03 15:53:31 +0200

    Update MiniCartData #15

--- common/app/common/pages/MiniCartData.java
+++ common/app/common/pages/MiniCartData.java
@@ -1,45 +1,23 @@
 package common.pages;
 
-import io.sphere.sdk.carts.Cart;
-import io.sphere.sdk.carts.CustomLineItem;
-import io.sphere.sdk.carts.LineItem;
 import io.sphere.sdk.models.Base;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MiniCartData extends Base {
 
-    final String text;
     final String url;
-    final long numItems;
+    final int numItems;
 
-    public MiniCartData(final Cart cart) {
-        this.text = "";
-        this.url = getUrlFromCart(cart);
-        this.numItems = getNumItemsFromCart(cart);
-    }
-
-    public String getText() {
-        return text;
+    public MiniCartData(final String url, final int numItems) {
+        this.url = url;
+        this.numItems = numItems;
     }
 
     public String getUrl() {
         return url;
     }
 
-    public long getNumItems() {
+    public int getNumItems() {
         return numItems;
-    }
-
-    private String getUrlFromCart(final Cart cart) {
-        return "";
-    }
-
-    private long getNumItemsFromCart(final Cart cart) {
-        return Stream.concat(
-                cart.getLineItems().stream().map(LineItem::getQuantity),
-                cart.getCustomLineItems().stream().map(CustomLineItem::getQuantity))
-                .mapToLong(i -> i).sum();
     }
 }
