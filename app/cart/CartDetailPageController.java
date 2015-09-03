da7d68c8d41472396f6062e81eed4c794383a7e5
schleichardt <michael.schleichardt@commercetools.de>
2015-09-03 09:24:54 +0200

    Refactor after merging master into feature branch. #4

--- app/cart/CartDetailPageController.java
+++ app/cart/CartDetailPageController.java
@@ -1,10 +1,8 @@
 package cart;
 
-import common.cms.CmsPage;
 import common.controllers.ControllerDependency;
 import common.controllers.SunriseController;
 import common.pages.SunrisePageData;
-import play.libs.F;
 import play.mvc.Result;
 
 import javax.inject.Inject;
@@ -16,12 +14,9 @@ public class CartDetailPageController extends SunriseController {
         super(controllerDependency);
     }
 
-    public F.Promise<Result> show(final String language) {
-        final F.Promise<CmsPage> commonCmsPagePromise = getCommonCmsPage();
-        return commonCmsPagePromise.map(commonCmsPage -> {
-            final CartDetailPageContent content = new CartDetailPageContent();
-            final SunrisePageData pageData = SunrisePageData.of(commonCmsPage, context(), content);
-            return ok(templateService().renderToHtml("cart", pageData));
-        });
+    public Result show(final String language) {
+        final CartDetailPageContent content = new CartDetailPageContent();
+        final SunrisePageData pageData = pageData(userContext(language), content);
+        return ok(templateService().renderToHtml("cart", pageData));
     }
 }
