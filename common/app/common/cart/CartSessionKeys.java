b8031db2fe4a00b0807e476b4c906434cfd106e2
Peter Gerhard <peter.gerhard@commercetools.de>
2015-09-04 17:29:38 +0200

    Remove public access to CartSessionKeys #15

--- common/app/common/cart/CartSessionKeys.java
+++ common/app/common/cart/CartSessionKeys.java
@@ -1,6 +1,6 @@
 package common.cart;
 
-public final class CartSessionKeys {
+final class CartSessionKeys {
     public static final String CART_ID = "cart-id";
     public static final String CART_ITEM_COUNT = "cart-item-count";
 
---
+++ common/app/common/cart/MiniCart.java
@@ -0,0 +1,4 @@
+package common.cart;
+
+public class MiniCart {
+}
